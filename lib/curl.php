1493409c4da982128ca2685cc0abeacba2e044fc
Dmitriy Buteiko <dmitriy.buteiko@mail.ru>
2017-11-04 07:41:44 +0200

    curl.php update
    Singleton patterns support added

--- lib/curl.php
+++ lib/curl.php
@@ -74,9 +74,27 @@ class Curl {
      * Sets the $cookie_file to "curl_cookie.txt" in the current directory
      * Also sets the $user_agent to $_SERVER['HTTP_USER_AGENT'] if it exists, 'Curl/PHP '.PHP_VERSION.' (http://github.com/shuber/curl)' otherwise
     **/
-    function __construct() {
+    private function __construct() {
         $this->cookie_file = dirname(__FILE__).DIRECTORY_SEPARATOR.'curl_cookie.txt';
         $this->user_agent = isset($_SERVER['HTTP_USER_AGENT']) ? $_SERVER['HTTP_USER_AGENT'] : 'Curl/PHP '.PHP_VERSION.' (http://github.com/shuber/curl)';
+    }
+    
+     /**
+     * Returns instance of Curl class
+     *
+     * @param string $url
+     * @param string $vars
+     * @return void
+     * @access protected
+    **/
+    public function getHelper()
+    {
+        static $instance = null;
+        
+        if($instance == null)
+        {
+            $instance = new Curl();
+        }
     }
     
     /**
@@ -252,5 +270,4 @@ class Curl {
             curl_setopt($this->request, constant('CURLOPT_'.str_replace('CURLOPT_', '', strtoupper($option))), $value);
         }
     }
-
 }
